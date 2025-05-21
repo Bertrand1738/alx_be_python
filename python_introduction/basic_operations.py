baf4c8db3577d51f15548a3c9486a413e6bbbd96
Bertrand1738 <hbertrand605@gmail.com>
2025-05-21 16:32:07 +0300

    Add basic_operations.py with basic arithmetic logic

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# variables declaration
+number1 = 10
+number2 = 5
+
+# Performing basic operations
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+# Print the results
+print("Addition of 10 and 5 is ",sum)
+print("Subtraction of 10 and 5 is ",difference)
+print("Multiplication of 10 and 5 is ",product)
